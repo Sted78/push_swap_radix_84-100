cc5c90fbbd6297af5bf3c47c0ca905c1cc003050
svanmarc <116564466+Sted78@users.noreply.github.com>
2023-10-02 12:12:09 +0200

    Add files via upload

---
+++ core/commands.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   commands.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svanmarc <@student.42perpignan.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 11:58:30 by svanmarc          #+#    #+#             */
+/*   Updated: 2023/10/02 12:10:56 by svanmarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+/*sa (swap a) swap les 2 1er de a
+sb (swap b) swap les 2 1er de b
+ss sa et sb en meme temps
+pa (push a) prend le 1er de b et le met sur a
+pb (push b) prend le 1er de a et le met sur b
+ra (rotate a) le 1er de a devient le dernier
+rb (rotate b) le 1er de b devient le dernier
+rr ra et rb en meme temps
+rra (reverse rotate a) le dernier de a devient le 1er
+rrb (reverse rotate b) le dernier de b devient le 1er
+rrr rra et rrb en meme temps*/
+
+void	swap_stack()
+{
+	
+}
+void	rotate_stack
+
